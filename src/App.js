92935f88e2ebb5122ba7b5f41dcd659c12d2aeba
Nguyễn Thị Minh Anh <minhanhnt.study@gmail.com>
2023-04-02 16:40:06 +0700

    Revert "Merge branch 'nganngan'"
    This reverts commit 9c44bc73b7bbd46b6f1f042aecf98b90655adaa0, reversing
    changes made to 15a2fe3d053ec437214426de31cefc07be339b24.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,140 @@
+
 import './App.css';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Routes, Route} from 'react-router-dom';
 
+import HeaderFilm from './components/HeaderFim';
+import FilmList from './components/FilmList';
+import UpComingFilm from './components/UpComingFilm';
+import Banner from './components/Banner';
+import Comment from './components/Comment';
+import Intro from './components/Intro';
+import Event from './components/Event';
+import News from './components/News';
 
-function App() {
-    return (
-        <div>
-            <Signup />
+import ViewFilmDetail from './components/View/ViewFilmDetail'
+import SeatBooking from './components/Booking/SeatBooking';
+const App = () => {
+  return (
+    
+  <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <HeaderFilm/>
            
-        </div>
-    );
+            </div>
+          }
+        >
+          </Route>
+       
+      </Routes>
+      <Routes>
+      <Route
+          path="/"
+          element={
+            <div>
+              <FilmList/>
+              
+            </div>
+          }
+        >
+        
+          </Route>
+            
+            <Route path="phimdangchieu" element=
+            {<div>
+              <HeaderFilm/>
+              <FilmList />
+            </div>}>
+            </Route>
+            <Route path="phimsapchieu" element={<div>
+              <HeaderFilm/>
+              <UpComingFilm />
+               </div>}>
+
+            </Route>
+      </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Banner/>
+              <Comment />
+           
+            </div>
+          }
+        >
+          </Route>
+       
+      </Routes>
+      <Routes>
+        <Route
+          path="/intro"
+          element={
+            <div>
+              <Intro/>
+           
+            </div>
+          }
+        >
+          </Route>
+       
+      </Routes>
+      <Routes>
+        <Route
+          path="/news"
+          element={
+            <div>
+              <News/>
+           
+            </div>
+          }
+        >
+          </Route>
+       
+      </Routes>
+      <Routes>
+        <Route
+          path="/thisfilm/:id"
+          element={
+            <div>
+             <ViewFilmDetail/>
+           
+            </div>
+          }
+        >
+          </Route>
+       
+      </Routes>
+      <Routes>
+      <Route
+          path="/seatbooking"
+          element={
+            <div>
+             <SeatBooking/>
+           
+            </div>
+          }
+        >
+          </Route>
+       
+      </Routes>
+    
+      <Footer />
+      
+    
+  </>
+
+
+  )
+    
 }
+
+
 export default App;
